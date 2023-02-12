667c25dfd9bad2e69d295e7e0f5883c7d19b66d3
Filipe Rodrigues <118074213+filiperodrigues07@users.noreply.github.com>
2023-02-12 14:38:44 -0300

    Delete ImersãoDEV directory

--- ImersãoDEV/Conversor de Moedas/conversorDeMoedas.js
+++
@@ -1,6 +0,0 @@
-var valorEmDolar = 1000;
-var cotacaoDoDolar = 5.15;
-
-var valorEmReal = valorEmDolar * cotacaoDoDolar;
-valorEmReal = valorEmReal.toFixed(2);
-alert("Olá Filipe o valor em reais é R$ " + valorEmReal);
